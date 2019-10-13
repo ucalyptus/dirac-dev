f0b9c5736dae83579b905be6bd1431ed39bd4afd
Sayantan Das <36279638+ucalyptus@users.noreply.github.com>
2019-10-13 14:55:24 +0530

    Code Formatted

--- Keyword_Extract.py
+++ Keyword_Extract.py
@@ -9,23 +9,32 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def keyword_extraction(tfidf_vector,document_list):
-    keywords=[]
-    for i in range(0,len(document_list)):
-        response2=tfidf_vector.transform([document_list[i]])
-        feature_array = np.array(tfidf_vector.get_feature_names())
-        tfidf_sorting = np.argsort(response2.toarray()).flatten()[::-1]
-        n=12
+def keyword_extraction(tfidf_vector, document_list):
+    keywords = []
+    for i in range(0, len(document_list)):
+        response2 = tfidf_vector.transform(
+            [document_list[i]]
+        )
+        feature_array = np.array(
+            tfidf_vector.get_feature_names()
+        )
+        tfidf_sorting = np.argsort(
+            response2.toarray()
+        ).flatten()[::-1]
+        n = 12
         top_n = feature_array[tfidf_sorting][:n]
         keywords.append(top_n)
     return keywords
 
+
 fileName = "report4.txt"
-d=open(fileName,"r",encoding='utf8')
-document=d.read()
-document_list=document.split("\n\n")
-tfidf_vector=TfidfVectorizer(stop_words='english',ngram_range=(1,3))
-response=tfidf_vector.fit_transform(document_list)
-keywords=keyword_extraction(tfidf_vector,document_list)
-keywords=np.asarray(keywords)
+d = open(fileName, "r", encoding="utf8")
+document = d.read()
+document_list = document.split("\n\n")
+tfidf_vector = TfidfVectorizer(
+    stop_words="english", ngram_range=(1, 3)
+)
+response = tfidf_vector.fit_transform(document_list)
+keywords = keyword_extraction(tfidf_vector, document_list)
+keywords = np.asarray(keywords)
 print(keywords)
